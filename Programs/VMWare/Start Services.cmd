f6e79900bafc1bf4dad410ad6373ab90f1311cc1
Joseph Plant <git@JosephPlant.com>
2015-05-08 07:26:29 +1000

    Add Scripts to start / stop VMWARE

---
+++ Programs/VMWare/Start Services.cmd
@@ -0,0 +1,6 @@
+net start "VMAuthdService"
+net start "VMnetDHCP"
+net start "VMware NAT Service"
+net start "VMUSBArbService"
+
+pause
---
+++ Programs/VMWare/Stop Services.cmd
@@ -0,0 +1,6 @@
+net stop "VMAuthdService"
+net stop "VMnetDHCP"
+net stop "VMware NAT Service"
+net stop "VMUSBArbService"
+
+pause
